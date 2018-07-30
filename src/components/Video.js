4e0f2ac033db73e97c07be91eb56d5a2fe5af833
damien <damien.savoldelli@gmail.com>
2018-07-30 18:57:52 +0200

    fix eslint format, add unique title iframe on Video component

--- src/components/Video.js
+++ src/components/Video.js
@@ -4,7 +4,14 @@ const YOUTUBE_EMBED = 'https://www.youtube.com/embed/';
 
 const Video = ({ videoId }) => (
   <div className="embed-responsive embed-responsive-16by9">
-    <iframe className="embed-responsive-item" src={`${YOUTUBE_EMBED}${videoId}?rel=0&amp`} frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen />
+    <iframe 
+      title={videoId}
+      className="embed-responsive-item" 
+      src={`${YOUTUBE_EMBED}${videoId}?rel=0&amp`} 
+      frameBorder="0" 
+      allow="autoplay; encrypted-media" 
+      allowFullScreen 
+    />
   </div>
 );
 
