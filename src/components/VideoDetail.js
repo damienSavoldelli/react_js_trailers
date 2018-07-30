821c382d3cac16569c14721adff3dd44182425cc
damien <damien.savoldelli@gmail.com>
2018-07-30 19:43:36 +0200

    fix undefined picture on video detail (404)

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -10,7 +10,9 @@ const VideoDetail = (props) => {
       <h1>{title}</h1>
       <div className="row">
         <div className="col-xs-4 text-center">
-          <img className="img-rounded img-fluid center-block" src={`${IMG_THUMB_BASE_URL}${picture}`} alt={title} />
+          { picture
+            && <img className="img-rounded img-fluid center-block" src={`${IMG_THUMB_BASE_URL}${picture}`} alt={title} />
+          }
         </div>
         <div className="col-xs-8">
           <p>{description}</p>
