2a2b6e196f9ef6ff9f12182e37ad162c508ecc12
damien <damien.savoldelli@gmail.com>
2018-07-30 19:41:46 +0200

    fix eslint format on Video component

--- src/components/Video.js
+++ src/components/Video.js
@@ -4,13 +4,13 @@ const YOUTUBE_EMBED = 'https://www.youtube.com/embed/';
 
 const Video = ({ videoId }) => (
   <div className="embed-responsive embed-responsive-16by9">
-    <iframe 
+    <iframe
       title={videoId}
-      className="embed-responsive-item" 
-      src={`${YOUTUBE_EMBED}${videoId}?rel=0&amp`} 
-      frameBorder="0" 
-      allow="autoplay; encrypted-media" 
-      allowFullScreen 
+      className="embed-responsive-item"
+      src={`${YOUTUBE_EMBED}${videoId}?rel=0&amp`}
+      frameBorder="0"
+      allow="autoplay; encrypted-media"
+      allowFullScreen
     />
   </div>
 );
